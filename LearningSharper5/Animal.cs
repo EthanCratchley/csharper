7d08fed46d2c0e0ac3bed21e2f05d669dd087823
Ethan Cratchley <138141942+EthanCratchley@users.noreply.github.com>
2023-11-03 19:35:19 -0700

    installment 5, moving onto projetcs

---
+++ LearningSharper5/Animal.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningSharper5
+{
+    internal class Animal
+    {
+        private string name; // if private not accessible by sub classes
+        private string sound;
+        public static int numOfAnimals = 0;
+        public const string SHELTER = "Derek's Home for Aniamls";
+        public readonly int idNum;
+
+        //Constructor:
+        public Animal() : this("No Name", "No Sound") { }
+
+       public Animal(string name) : this(name, "No Sound") { }
+        
+       public Animal(string name, string sound)
+        {
+            SetName(name);
+            Sound = sound;
+
+            NumOfAnimals = 1;
+            Random rnd = new Random();
+            idNum = rnd.Next(1, 2147483640);
+        }
+        
+        
+        
+        /* public Animal(string name = "No Name")
+        {
+            this.name = name;
+            this.sound = "No Sound";
+            numOfAnimals++;
+        }
+
+        public Animal(string name = "No Name", string sound = "No Sound") : this(name)
+        {
+            this.name = name;
+            this.sound = sound;
+            numOfAnimals++;
+        }*/
+
+        public void MakeSound()
+        {
+            Console.WriteLine("{0} says {1}", name, sound);
+        }
+
+        public static int GetNumAnimals()
+        {
+            return numOfAnimals;
+        }
+
+        public void SetName(string name)
+        {
+            if(!name.Any(char.IsDigit))
+            {
+                this.name = name;
+            }
+            else
+            {
+                this.name = "No Name";
+                Console.WriteLine("Name can't contain numers");
+            }
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public string Sound
+        {
+            get { return sound; }
+            set
+            {
+                if (value.Length > 10)
+                {
+                    sound = "No Sound";
+                    Console.WriteLine("Sound is toom long");
+                }
+                else
+                {
+                    sound = value;
+                }
+            }
+        }
+
+        public string Owner { get; set; } = "No Owner";
+
+        public string int NumOfAnimals
+        {
+            get { return numOfAnimals; }
+            set { numOfAnimals += value; }
+        }
+    }
+}
---
+++ LearningSharper5/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LearningSharper5
+{
+    public class Program
+    {
+        // FUNCTIONS
+
+        /*static void PaintCar(CarColor cc)
+        {
+            Console.WriteLine("The car was painted {0} with the code {1}",
+                cc, (int)cc);
+        }*/
+
+
+
+        // END OF FUNCTIONS
+
+        // Enumerate Types
+        /*enum CarColor : byte
+        {
+            Orange = 1,
+            Blue,
+            Green,
+            Red,
+            Yellow
+        }*/
+
+        static void Main(string[] args)
+        {
+            Rectangle rect1;
+            rect1.length = 200;
+            rect1.width = 50;
+            Console.WriteLine("Area of rect1 : {0}",
+                rect1.Area());
+            Rectangle rect2 = new Rectangle(100, 40);
+            rect2 = rect1;
+            rect1.length = 33;
+            Console.WriteLine("Rect2.length : {0}",
+                rect2.length);
+
+            Animal fox = new Animal()
+            {
+                name = "Red",
+                sound = "Raaw"
+        };
+
+            Console.WriteLine("# of Animals {0}",
+                Animal.GetNumAnimals());
+
+            Console.WriteLine("Area of Rectangle : {0}",
+                ShapeMath.GetArea("rectangle", 5, 6));
+
+            int? randNum = null;
+            if (randNum == null)
+            {
+                Console.WriteLine("randNum is null");
+            }
+
+            if(!randNum.HasValue)
+            {
+                Console.WriteLine("randNum is null");
+            }
+
+        }
+        /*// Enumerated Types:
+            CarColor car1 = CarColor.Blue;
+            PaintCar(car1);*/
+
+        struct Rectangle
+        {
+            public double length;
+            public double width;
+
+            public Rectangle(double l = 1, double w = 1) { length = l; width = w; }
+            public double Area()
+            {
+                return length * width;
+            }
+        }
+    }
+}    
